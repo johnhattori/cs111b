acabb3c34400f30901030868650c14657ef65e82
john hattori <johnmhattori@yahoo.com>
2014-09-05 18:34:31 -0700

    add hw02

---
+++ hw02/List.java
@@ -0,0 +1,23 @@
+/*
+ 
+ Author: jhattori 
+ Date  : 09/05/14
+ Program Name: List.java
+ Objective: This program does nothing.
+ 
+*/
+ 
+//import java.awt.*;
+//import java.applet.*;
+//import java.util.*;
+//import java.io.*;
+//import javax.swing.*;
+ 
+class List
+{
+   public static void main(String args[])
+   {
+   
+     // put your code here
+   }
+}
