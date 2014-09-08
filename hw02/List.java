5340d32fef6a11eaffc918c95c2e1ae4b4510a0e
john hattori <johnmhattori@yahoo.com>
2014-09-08 11:53:26 -0700

    List.java functional without -n option

--- hw02/List.java
+++ hw02/List.java
@@ -3,21 +3,46 @@
  Author: jhattori 
  Date  : 09/05/14
  Program Name: List.java
- Objective: This program does nothing.
+ Objective: This program will display the contents of a file
+            entered as a command line argument. If option -n
+            is arg[0] the program will display the line number.
  
 */
  
 //import java.awt.*;
 //import java.applet.*;
-//import java.util.*;
-//import java.io.*;
+import java.util.*;
+import java.io.*;
 //import javax.swing.*;
  
 class List
 {
-   public static void main(String args[])
-   {
    
-     // put your code here
+    public static void die(String msg)
+    {
+         System.err.println(msg);
+         System.exit(1);
+    }
+
+    public static void main(String args[])
+   {
+        File f = new File(args[0]);
+        
+        if (!f.exists()) die("file does not exist");
+        if (f.isDirectory()) die("is a directory");
+        if (!f.isFile()) die("file  irregular");
+        if (!f.canRead()) die("file not readable.");     
+                
+        try
+        {
+             String line;
+             Scanner sc = new Scanner(f);
+             while(sc.hasNext())
+             {
+                  line = sc.nextLine();
+                  System.out.println(line);
+             }
+        }catch(FileNotFoundException e){}
+
    }
 }
